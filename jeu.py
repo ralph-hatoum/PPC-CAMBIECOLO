76cf8e3acc5ab00d469da41c52e9f7fdacbb1d25
HillelPa <64850372+HillelPa@users.noreply.github.com>
2022-01-05 09:46:54 +0100

    dgfza

--- jeu.py
+++ jeu.py
@@ -22,18 +22,16 @@ playing_lock = threading.Lock()
 def player(id):
     def message_receiver(id):
         while True:
-            message, type = mq.receive(type=id)
+            message, _ = mq.receive()
             message = message.decode()
             print(message)
+
 
     message_receiver = threading.Thread(target=message_receiver, args=(id,))
     message_receiver.start()
 
     # Cartes du joueur
     cards = ["plane", "car", "car", "train", "plane"]
-
-    # Liste pour garder en mémoire les offres du joueur
-    own_offers = []
 
     # Intéraction avec le joueur
 
@@ -49,11 +47,11 @@ def player(id):
         if interaction == "make_offer":
             pattern = input("Entrez le motif que vous voulez échanger ")
             number = int(input("Entrez le nombre de cartes "))
-            make_offer(cards, pattern, number, own_offers)
+            make_offer(cards, pattern, number, id)
 
         if interaction == "accept_offer":
             number_id = input("Entrez l'identifiant de l'offre ")
-            accept_offer(int(number_id), cards, own_offers)
+            accept_offer(int(number_id), cards, id)
 
         if interaction == "display_offers":
             display_offers()
@@ -92,7 +90,7 @@ def display_locks():
     print(offers_locks)
 
 
-def make_offer(card_list, pattern, number_of_cards, own_offers):
+def make_offer(card_list, pattern, number_of_cards, id_player):
 
     # On vérifie d'abord que l'offre ne contient pas plus de 3 cartes (contrainte donnée dans le sujet)
     if number_of_cards > 3:
@@ -100,17 +98,19 @@ def make_offer(card_list, pattern, number_of_cards, own_offers):
         return None
     # Ensuite, on vérifie que le joueur a bien les cartes qu'il veut échanger
     k = 0
+
     # On compte les cartes du motif donné dans le jeu du joueur
     for i in card_list:
         if i == pattern:
             k += 1
+
     # On teste si le nombre de cartes offertes est inféreur ou égal au nombre de cartes du même motif présentes dans le jeu du joueur
     test = number_of_cards <= k
     # Si c'est le cas, alors on peut échanger les cartes
     if test:
         offer_lock.acquire()
         # On code l'offre, sous la forme "motif, nombre_de_cartes"
-        offer = pattern + "," + str(number_of_cards)
+        offer = pattern + "," + str(number_of_cards) + "," + id_player + ", OFFER"
 
         # On crée un identifiant d'offre
         id = len(offers) + 1
@@ -121,9 +121,6 @@ def make_offer(card_list, pattern, number_of_cards, own_offers):
         # On crée un lock pour cette offre
         offers_locks[id] = threading.Lock()
 
-        # On ajoute l'identifiant de l'offre dans les propres offres du joueur
-        own_offers.append(id)
-
         # On imprime la liste des offres pour bien montrer au joueur que son offre est ajoutée
         print(offers)
     else:
@@ -131,19 +128,19 @@ def make_offer(card_list, pattern, number_of_cards, own_offers):
         print("You can't do that")
 
 
-def accept_offer(offer_id, card_list, own_offers):
+def accept_offer(offer_id, card_list, id_player):
 
     # On commence par récupérer l'offre
     offers_locks[offer_id].acquire()
     offer = offers[offer_id]
 
-    # On vérifie que le joueur n'accepte pas sa propre offre
-    if offer_id in own_offers:
-        print("Vous ne pouvez pas accepter votre propre offre")
-        return None
-
     # On transforme la chaine de caractère qui représente l'offre en liste de la forme [motif, nb_cartes]
     offer = offer.split(",")
+
+    # On vérifie que le joueur n'accepte pas sa propre offre
+    if id_player == offer[2]:
+        print("Vous ne pouvez pas accepter votre propre offre")
+        return None
 
     # On récupère le nombre de cartes
     nb_cards = int(offer[1])
@@ -162,7 +159,14 @@ def accept_offer(offer_id, card_list, own_offers):
 
     # test
     if test:
-
+        for i in range(nb_cards):
+            if card_list[i] == pattern_to_exchange:
+                card_list.remove(i)
+        print(card_list)
+        for i in range(nb_cards):
+            card_list.append(offer[0])
+        print(card_list)
+        mq.send(offer_id+", Accepted")
 
     return "Offer accepted"
 
