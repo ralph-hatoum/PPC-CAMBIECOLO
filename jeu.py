a04130688892f18eb9fc366d50390b5fdf4c5f92
HillelPa <64850372+HillelPa@users.noreply.github.com>
2022-01-05 10:22:42 +0100

    testeur

--- jeu.py
+++ jeu.py
@@ -1,5 +1,7 @@
 import random
 import threading
+import time
+
 import sysv_ipc
 
 key = 129
@@ -26,32 +28,33 @@ def player(id):
         for i in range(mes[3]):
             if card_list[i] == mes[1]:
                 card_list.remove(i)
-        print(card_list)
+        print(card_list, "id :", id)
         for i in range(mes[3]):
             card_list.append(mes[2])
-        print(card_list)
+        print(card_list, "id :", id)
 
     def message_receiver(id, card_list):
         while True:
             message, _ = mq.receive()
             message = message.decode()
-            print(message)
+            print(message, "id :", id)
             if message.startswith("Accepted"):
                 finisher = threading.Thread(target=finish_deal, args=(message, card_list))
                 finisher.start()
 
-
-
-    message_receiver = threading.Thread(target=message_receiver, args=(id, card_list))
-    message_receiver.start()
-
     # Cartes du joueur
     cards = ["plane", "car", "car", "train", "plane"]
+
+    message_receiver = threading.Thread(target=message_receiver, args=(id, cards))
+    message_receiver.start()
 
     # Intéraction avec le joueur
 
     while playing:
-        interaction = input("Que voulez vous faire ? ")
+        interaction = input("Que voulez vous faire ? id :" + str(id))
+
+        if interaction == "sleep":
+            time.sleep(1)
 
         if interaction == "ring_bell":
             ring_bell(cards, id, playing)
@@ -60,12 +63,12 @@ def player(id):
             display_cards(cards)
 
         if interaction == "make_offer":
-            pattern = input("Entrez le motif que vous voulez échanger ")
-            number = int(input("Entrez le nombre de cartes "))
+            pattern = input("Entrez le motif que vous voulez échanger id :" + str(id))
+            number = int(input("Entrez le nombre de cartes id :" + str(id)))
             make_offer(cards, pattern, number, id)
 
         if interaction == "accept_offer":
-            number_id = input("Entrez l'identifiant de l'offre ")
+            number_id = input("Entrez l'identifiant de l'offre id :" + str(id))
             accept_offer(int(number_id), cards, id)
 
         if interaction == "display_offers":
@@ -89,7 +92,7 @@ def ring_bell(card_list, player, playing):
         playing = False
     # Sinon, on signale que le joueur n'a pas 5 cartes identiques, le jeu reprend
     else:
-        print("Vous n'avez pas 5 cartes identiques")
+        print("Vous n'avez pas 5 cartes identiques", "id :", id)
     playing_lock.release()
 
 
@@ -109,7 +112,7 @@ def make_offer(card_list, pattern, number_of_cards, id_player):
 
     # On vérifie d'abord que l'offre ne contient pas plus de 3 cartes (contrainte donnée dans le sujet)
     if number_of_cards > 3:
-        print("You can't do that")
+        print("You can't do that", "id :", id_player)
         return None
     # Ensuite, on vérifie que le joueur a bien les cartes qu'il veut échanger
     k = 0
@@ -125,7 +128,7 @@ def make_offer(card_list, pattern, number_of_cards, id_player):
     if test:
         offer_lock.acquire()
         # On code l'offre, sous la forme "motif, nombre_de_cartes"
-        offer = pattern + "," + str(number_of_cards) + "," + id_player + ", OFFER"
+        offer = pattern + "," + str(number_of_cards) + "," + str(id_player) + ", OFFER"
 
         # On crée un identifiant d'offre
         id = len(offers) + 1
@@ -140,7 +143,7 @@ def make_offer(card_list, pattern, number_of_cards, id_player):
         print(offers)
     else:
         # Si les condition n'étaient pas vérifiées, on ne peut pas faire l'échange
-        print("You can't do that")
+        print("You can't do that", "id :", id_player)
 
 
 def accept_offer(offer_id, card_list, id_player):
@@ -154,14 +157,15 @@ def accept_offer(offer_id, card_list, id_player):
 
     # On vérifie que le joueur n'accepte pas sa propre offre
     if id_player == offer[2]:
-        print("Vous ne pouvez pas accepter votre propre offre")
+        print("Vous ne pouvez pas accepter votre propre offre", "id :", id_player)
         return None
 
     # On récupère le nombre de cartes
     nb_cards = int(offer[1])
 
     # On demande au joueur d'entrer les cartes à échanger (séparées par des espaces)
-    pattern_to_exchange = input("Entrez le motif à echanger")
+    print("id :", id_player)
+    pattern_to_exchange = input("Entrez le motif à echanger id :" + str(id_player))
 
     # On compte le nombre de carte du motif que le joueur veut echanger
     cards_counter = 0
@@ -186,4 +190,7 @@ def accept_offer(offer_id, card_list, id_player):
     return "Offer accepted"
 
 
-player(1)
+if __name__ == "__main__":
+    players = [(threading.Thread(target=player, args=(i,))) for i in range(2)]
+    for p in players:
+        p.start()
