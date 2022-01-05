58899172ed26b279b07fae98cd1417c50227c01e
ralph <ralph-hatoum@insa-lyon.fr>
2022-01-05 09:55:32 +0100

    Finished distribution algorithm

--- distribution_cartes.py
+++ distribution_cartes.py
@@ -1,6 +1,6 @@
 import random
 
-nb_joueurs = 3
+nb_joueurs = 5
 
 motifs = ["plane", "car", "train", "bike", "shoes"]
 
@@ -13,3 +13,9 @@ for i in motifs:
         tas_de_cartes.append(i)
 
 cartes = [[] for i in range(nb_joueurs)]
+
+for i in range(nb_joueurs):
+    for j in range(5):
+        cartes[i].append(motifs[random.randint(0, len(motifs) - 1)])
+
+print(cartes)
