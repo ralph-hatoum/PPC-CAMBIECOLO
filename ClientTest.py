6577d414640e9d5bbcb92a2a2abf42997c7fed38
ralph <ralph-hatoum@insa-lyon.fr>
2022-01-12 09:22:46 +0100

    added client interface

--- ClientTest.py
+++ ClientTest.py
@@ -26,5 +26,12 @@ while True:
 
 if test == True:
     print("Je suis accepté dans la partie")
+    print("En attente que le jeu commence")
+
+    print(
+        "\033[91m"
+        + "Mode d'emploi : \n Vous avez accès aux commandes : \n ring_bell pour sonner la cloche \n make_offer pour faire une offre \n accept_offer pour accepter une offre \n display offers pour afficher les offres"
+        + "\033[0m"
+    )
 else:
     print("NO")
